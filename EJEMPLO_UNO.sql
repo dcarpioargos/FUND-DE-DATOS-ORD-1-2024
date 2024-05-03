6dfb28b549414a1c4cedef7bb339598aba463260
carpiordaniel <carpiordaniel@gmail.com>
2024-05-02 22:17:59 -0500

    insert tablas

--- EJEMPLO_UNO.sql
+++ EJEMPLO_UNO.sql
@@ -4,9 +4,9 @@
 -- //COMANDOS
 -- //CREAR BASE DE DATOS
 
--- CREATE DATABASE materia_fdb
+CREATE DATABASE materia_fdb
 -- // definir por defecto db
--- USE materia_fdb
+USE materia_fdb
 
 -- crear tablas
 /*CREATE TABLE NOMBRE_TABLA (
@@ -26,8 +26,18 @@ CREATE TABLE AUTOS(
     IsDisponible BOOLEAN
 );
 
-
+-- CONSULTAR TABLA
 SELECT * FROM autos
+-- insertar datos en tabla de forma individual
+INSERT INTO autos (PLACA, MODELO, MARCA, COLOR, CANT_NEUMATICO, IsDisponible) 
+			VALUES ("ABC-1234", "AVEO", "CHEVROLET", "ROJO", 5, true)
+
+-- insertar datos en tabla de forma grupal            
+INSERT INTO autos (PLACA, MODELO, MARCA, COLOR, CANT_NEUMATICO, IsDisponible) 
+VALUES ("XYZ-5432", "AVEO", "CHEVROLET", "ROJO", 5, true),
+	   ("JKL-1524", "RENAULT MODEL", "RENAULT", "AZUL", 4, false),
+	   ("OIP-1523", "KIA MODEL", "KIA", "AMARILLO", 6, true)
+
 
 
 
