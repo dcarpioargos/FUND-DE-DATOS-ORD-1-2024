98ccba6df324b8d9ba05f20fd5a9e337daad75e5
carpiordaniel <carpiordaniel@gmail.com>
2024-05-07 23:00:15 -0500

    update

--- EJEMPLO_UNO.sql
+++ EJEMPLO_UNO.sql
@@ -3,7 +3,7 @@
 
 -- //COMANDOS
 -- //CREAR BASE DE DATOS
-
+-- SYNTAXIS // REGLAS DE ESCRITURA O LOS PASOS A SEGUIR
 CREATE DATABASE materia_fdb
 -- // definir por defecto db
 USE materia_fdb
@@ -17,7 +17,7 @@ USE materia_fdb
 );*/
 
 CREATE TABLE AUTOS(
-	-- ID INT AUTO_INCREMENT,
+	ID INT AUTO_INCREMENT PRIMARY KEY,
     PLACA VARCHAR(8),
     MODELO VARCHAR(30),
     MARCA VARCHAR(50),
@@ -26,6 +26,18 @@ CREATE TABLE AUTOS(
     IsDisponible BOOLEAN
 );
 
+0	11	21:50:16	CREATE TABLE AUTOS(
+  ID INT AUTO_INCREMENT,
+     PLACA VARCHAR(8),
+     MODELO VARCHAR(30),
+     MARCA VARCHAR(50),
+     COLOR VARCHAR (40),
+     CANT_NEUMATICO INT,
+     IsDisponible BOOLEAN
+ )	Error Code: 1075. Incorrect table definition; 
+ there can be only one auto column and it must be 
+ defined as a key	0.000 sec
+ 
 -- CONSULTAR TABLA
 SELECT * FROM autos
 -- insertar datos en tabla de forma individual
@@ -37,11 +49,29 @@ INSERT INTO autos (PLACA, MODELO, MARCA, COLOR)
 VALUES ("XYZ-9999", "AVEO", "CHEVROLET", 125),
 	   ("JKL-1524", "RENAULT MODEL", "RENAULT", "AZUL"),
 	   ("OIP-1523", "KIA MODEL", "KIA", "AMARILLO")
+       
+use materia_fdb
+-- SELECT * FROM autos--  where ID = 7
 
+-- OIP-1523
+UPDATE autos
+SET IsDisponible = false
+	-- CANT_NEUMATICO = 4
+WHERE ID >= 1 AND CANT_NEUMATICO = 5
 
-    
-    
-    
+0	29	22:10:06	UPDATE autos
+ SET IsDisponible = false,
+  -- CANT_NEUMATICO = 4
+ WHERE ID >= 1 AND CANT_NEUMATICO = 5	
+ Error Code: 1064. You have an error in your SQL s
+ yntax; check the manual that corresponds to 
+ your MySQL server version for the right syntax 
+ to use near 'WHERE ID >= 1 AND CANT_NEUMATICO = 5' 
+ at line 4	0.000 sec
+ 
+
+ 
+
 
 
 
