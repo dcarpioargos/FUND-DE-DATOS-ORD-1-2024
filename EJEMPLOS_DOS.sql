00e659223cf806de723fee0264c0ea39b9a51de0
carpiordaniel <carpiordaniel@gmail.com>
2024-05-21 22:12:09 -0500

    update like in

--- EJEMPLOS_DOS.sql
+++ EJEMPLOS_DOS.sql
@@ -6,6 +6,25 @@ where university = "Universidad de San Andres";
 
 
 
+/*SELECT * FROM mock_data
+where university LIKE "%ka%";*/
+
 
 SELECT * FROM mock_data
-where university LIKE "%University"
+where id not in(1, 2, 3, 4)
+
+
+SELECT * FROM mock_data
+where id not in(1, 2, 3, 4)
+ORDER BY first_name DESC
+
+
+/*SELECT * FROM materia_db.mock_data
+where id between 10 and 18*/
+/*SELECT * FROM materia_db.mock_data
+where first_name LIKE "%a%"*/
+
+SELECT * FROM materia_db.mock_data
+where first_name in (
+"Vaclav","Ky")
+
